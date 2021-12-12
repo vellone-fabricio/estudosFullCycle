86feb1c6c6903ed5b1048c4fc3ca06eea13ba3a9
Vellone <fabriciovellone@gmail.com>
2021-12-12 15:15:09 -0300

    Init aula 3 - adding sqlite and entities

---
+++ nestjs/src/orders/entities/order.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  DataType,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
+
+export enum OrderStatus {
+  PENDING = 'pending',
+  APPROVED = 'approved',
+}
+
+@Table({
+  tableName: 'orders',
+  createdAt: 'created_at',
+  updatedAt: 'updated_at',
+})
+export class Order extends Model {
+  @PrimaryKey
+  @Column({ type: DataType.UUIDV4, defaultValue: DataType.UUIDV4 })
+  id: string;
+
+  @Column({ allowNull: false, type: DataType.DECIMAL(10, 2) })
+  amout: number;
+
+  @Column({ allowNull: false })
+  credit_card_number: string;
+
+  @Column({ allowNull: false })
+  credit_card_name: string;
+
+  @Column({ allowNull: false, defaultValue: OrderStatus.PENDING })
+  status: OrderStatus;
+}
